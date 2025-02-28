e0548facfcc73c50a3ba6a88a5c8fa898a317f43
Robertjsr <Eryero02@gmail.com>
2025-02-28 11:12:11 +0000

    G.excusas-Corregido

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  function generadorDeExcusas() {
+    let who = ['Un pollito', 'Mi gato', 'Mi vecino', 'El profe Pool'];
+    let action = ['desintegro', 'devoro', 'escondio', 'deleteo'];
+    let what = ['mi proyecto', 'mi portatil', 'tu secreto', 'tus esperanzas'];
+    let when = ['antes del trabajo', 'cuando estaba durmiendo', 'mientras me estaba en la ducha', 'durante mi almuerzo'];
+  
+    let text1 = who[Math.floor(Math.random() * who.length)]
+    let text2 = action[Math.floor(Math.random() * action.length)]
+    let text3 = what[Math.floor(Math.random() * what.length)]
+    let text4 = when[Math.floor(Math.random() * when.length)]
+  
+    let resultado = text1 + " "  + text2 + " " + text3 + " " + text4;
+    
+    document.getElementById("excuse").innerHTML = resultado
+  }
+  generadorDeExcusas()
+}
